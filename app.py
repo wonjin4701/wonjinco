5071c13a16417fa1079e888dedce420300467ca2
wonjin4701 <62679808+wonjin4701@users.noreply.github.com>
2020-06-03 16:35:30 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,107 @@
+
+
+import discord
+import asyncio
+
+import option
+
+app = discord.Client()
+
+class DPNK:
+	def __init__(self):
+		self.text = ""
+		self.channels = []
+		
+		self.success = []
+		self.failed = []
+		self.channel_cant_make = []
+	
+	async def get_announce_channel(self):
+		for guild in app.guilds:
+			Allow = False
+			for channel in guild.channels:
+				if guild.id in [channel.guild.id for channel in self.channels]: break
+				
+				for Prefix in option.allowprefix:
+					if Prefix in channel.name:
+						Allow = True
+				
+				for Prefix in option.disallowprefix:
+					if Prefix in channel.name:
+						Allows = False
+				
+				if Allow and isinstance(channel, discord.TextChannel) and not channel in self.channels:
+					self.channels.append(channel);break;
+			
+			if option.nfct and not guild.id in [channel.guild.id for channel in self.channels]:
+				channel = await self.create_announce_channel(guild)
+				if channel:
+					self.channels.append(channel)
+	
+	async def create_announce_channel(self, guild):
+		try:
+			return await guild.create_text_channel(option.nfctname, reason="Because of DPNK")
+		except Exception as e:
+			self.channel_cant_make.append((guild, e))
+			return None
+	
+	def make_tasks(self):
+		return [self.send(channel) for channel in self.channels]
+	
+	async def send(self, channel):
+		try:
+			self.success.append(await channel.send(self.text))
+		except Exception as e:
+			self.failed.append((channel, e))
+	
+	async def announce(self, text):
+		self.text = text
+		await self.get_announce_channel()
+		
+		await asyncio.wait(self.make_tasks())
+		
+@app.event
+async def on_ready():
+	print(f"원진#7917 / {app.user} ({app.user.id})")
+
+async def process_command(message):
+	if message.content.startswith(option.command) and message.author.id in option.owner:
+		notice_text = message.content[len(option.command):]
+		
+		embed = discord.Embed(title="원진#7917", color=0xfcf794)
+		msg = await message.channel.send(message.author.mention, embed=embed)
+		
+		dpnk = DPNK()
+		await dpnk.get_announce_channel()
+		
+		embed = discord.Embed(title="원진#7917", color=0xb4fc94, description="발신중 입니다")
+		await msg.edit(embed=embed)
+		
+		await dpnk.announce(notice_text)
+		await msg.delete()
+		
+		escape_drop = '\n'
+		
+		await message.channel.send(f"""**✅ 공지를 발신하였습니다
+
+공지 발신 성공 채널:
+`{len(dpnk.success)}`개의 채널에 송신하였습니다.
+
+공지 발신 실패 채널:
+```
+{escape_drop.join([f'{channel.name} [{type(error)}]' for channel, error in dpnk.failed])} 
+```
+
+공지 채널 생성 실패:
+```
+{escape_drop.join([f'{guild.name} [{type(error)}]' for guild, error in dpnk.channel_cant_make])} 
+```**
+
+__원진#7917__""")
+
+@app.event
+async def on_message(message):
+	await process_command(message)
+
+if __name__ == '__main__':
+	app.run(option.token)
---
+++ option.py
@@ -0,0 +1,27 @@
+
+
+#공지할 사람의 디스코드 ID 입력
+owner = [686560700562407424]
+#봇의 토큰 입력
+token = "NzE3MzAxMjMxMjIzMzA4MzI5.Xtbveg.7ZJNe9UvOWpYGxvvqAukuc-N_rg"
+#봇의 접두사 입력
+first = "/"
+#봇의 공지 명령어 입력
+no = "공지"
+
+command = first + no
+
+#공지 채널을 찾을 수 없을 시
+nfct = True # ( True : 채널 생성후 발송, False : 아무것도 안함 )
+#생성할 채널 이름
+nfctname = "공지"
+
+""" 공지 채널 설정입니다. (자신없으면 기본으로) 반드시 List 형이여야 합니다. """
+
+#허용 공지 채널 접두사
+allowprefix = ["notice", "공지"]
+
+#허용 공지 채널 접두사가 들어있다 하더라도 이 접두사가 들어가 있으면 공지 하지 않습니다.
+disallowprefix = ["밴", "경고", "제재", "길드", "ban", "worry", "warn", "guild"]
+
+__Do_Per_Task = 5
